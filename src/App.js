d75730eebe6f00761a502c8ae5ac8c36a76ecbeb
noman <nomannabil30@gmail.com>
2022-10-10 22:48:01 +0600

    navbar layout added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Header from './Components/Header/Header';
+import Navbar from './Layouts/Navbar/Navbar';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Navbar />
+			<Header />
+		</div>
+	);
 }
 
 export default App;
