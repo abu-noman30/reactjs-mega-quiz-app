f77c1d554a898db6cea1246a7bf575f8def31820
noman <nomannabil30@gmail.com>
2022-10-11 21:42:33 +0600

    In Blog page questions and answers added successfully

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -26,14 +26,15 @@ font-bold
 '
 								type='button'
 							>
-								Q1: How Does React Works?
+								Q1: What is the purpose of react router?
 							</button>
 						</h2>
 
 						<div className='py-4 px-5'>
-							<strong>Ans:</strong> React creates a VIRTUAL DOM in memory.Instead of manipulating the browser's DOM directly, React creates a virtual DOM in
-							memory, where it does all the necessary manipulating, before making the changes in the browser DOM.React finds out what changes have been made,
-							and changes only what needs to be changed.
+							<strong>Ans:</strong> React Router is a JavaScript library used to handle client and server-side routing in React applications. It allows the
+							creation of single-page web or mobile apps that allows navigation without the page refreshing. React Router also gives us access to browser
+							history features while maintaining the correct view of the application.React Router uses a component-based approach to routing. It provides
+							different routing components as needed by the application.
 						</div>
 					</div>
 					<div className=' bg-white border border-gray-200'>
@@ -57,14 +58,18 @@ font-bold
 '
 								type='button'
 							>
-								Q1: How Does React Works?
+								Q1: How does Context API works?
 							</button>
 						</h2>
 
 						<div className='py-4 px-5'>
-							<strong>Ans:</strong> React creates a VIRTUAL DOM in memory.Instead of manipulating the browser's DOM directly, React creates a virtual DOM in
-							memory, where it does all the necessary manipulating, before making the changes in the browser DOM.React finds out what changes have been made,
-							and changes only what needs to be changed.
+							<strong>Ans:</strong> The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is
+							the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on.
+							<br />
+							<br />
+							React.createContext() is all need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to
+							its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a
+							component that consumes and uses the state.
 						</div>
 					</div>
 					<div className=' bg-white border border-gray-200'>
@@ -88,14 +93,23 @@ font-bold
 '
 								type='button'
 							>
-								Q1: How Does React Works?
+								Q1: What is useRef hook in react?
 							</button>
 						</h2>
 
 						<div className='py-4 px-5'>
-							<strong>Ans:</strong> React creates a VIRTUAL DOM in memory.Instead of manipulating the browser's DOM directly, React creates a virtual DOM in
-							memory, where it does all the necessary manipulating, before making the changes in the browser DOM.React finds out what changes have been made,
-							and changes only what needs to be changed.
+							<strong>Ans:</strong> useRef() hook is part of the React Hooks API. It is a function which takes a maximum of one argument and returns an Object
+							called current. The returned object has a property called current whose value is the argument passed to useRef(). If you invoke it without an
+							argument, the returned object's current property is set to undefined..
+							<br />
+							<br />
+							Some of the use cases of useRef hook are:
+							<br />
+							<ul className='list-decimal pl-8'>
+								<li>To access DOM elements.</li>
+								<li>To persist values in successive renders.</li>
+								<li>It can be used to store a mutable value that does not cause a re-render when updated.</li>
+							</ul>
 						</div>
 					</div>
 				</div>
